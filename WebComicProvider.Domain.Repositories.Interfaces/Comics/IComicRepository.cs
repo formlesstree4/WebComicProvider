80259cfc8c5409d289a8ea27dbcb7747d5c5c004
Adam Sears <asears2@live.com>
2022-04-20 02:46:38 -0500

    Documentation for IComicRepository

--- WebComicProvider.Domain.Repositories.Interfaces/Comics/IComicRepository.cs
+++ WebComicProvider.Domain.Repositories.Interfaces/Comics/IComicRepository.cs
@@ -4,7 +4,17 @@ namespace WebComicProvider.Domain.Repositories.Interfaces
 {
     public interface IComicRepository
     {
+        /// <summary>
+        /// Gets the specific details of a particular comic
+        /// </summary>
+        /// <param name="comicId">The ID of the comic</param>
+        /// <returns>A tuple that contains the full comic model and a key-value pair of issues and pages</returns>
         Task<(ComicModel, Dictionary<IssueModel, IEnumerable<PageModel>>)> Get(int comicId);
+
+        /// <summary>
+        /// Gets all the comics available
+        /// </summary>
+        /// <returns>A collection of comic book entities</returns>
         Task<IEnumerable<ComicModel>> GetAllComics();
     }
 }
