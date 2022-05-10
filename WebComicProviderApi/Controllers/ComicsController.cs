3ede608332dad8653a37a9c8d50a6c6bf61c6e55
Adam Sears <asears2@live.com>
2022-05-10 02:10:03 -0500

    Added a lot of new methods to the Comic controller

--- WebComicProviderApi/Controllers/ComicsController.cs
+++ WebComicProviderApi/Controllers/ComicsController.cs
@@ -11,7 +11,7 @@ namespace WebComicProviderApi.Controllers
     {
         private readonly IComicsManager comicsManager;
 
-        public ComicsController(IComicsManager comicsManager)
+        public ComicsController(IComicsManager comicsManager, IImageManager imageManager)
         {
             this.comicsManager = comicsManager;
         }
@@ -28,6 +28,54 @@ namespace WebComicProviderApi.Controllers
         public async Task<IActionResult> GetComicDetails(int comicId)
         {
             return Ok(await comicsManager.GetComicDetails(comicId));
+        }
+
+        [HttpGet("{comicId:int}/cover")]
+        public async Task<IActionResult> GetComicCover(int comicId)
+        {
+            var details = await comicsManager.GetComicDetails(comicId);
+            
+            //return await Task.FromResult(NotImplemented());
+        }
+
+        [HttpGet("{comicId:int}/{issueId:int}/{pageId:int}/image")]
+        public async Task<IActionResult> GetPageImage(int comicId, int issueId, int pageId)
+        {
+            return await Task.FromResult(NotImplemented());
+        }
+
+
+
+
+        [HttpPost("create"), Authorize]
+        public async Task<IActionResult> CreateComic()
+        {
+            if (!Request.HasFormContentType)
+            {
+                return BadRequest();
+            }
+
+            var formCollection = await Request.ReadFormAsync();
+
+            foreach (var file in formCollection.Files)
+            {
+                
+            }
+
+
+            return Ok();
+        }
+
+        [HttpPut("update"), Authorize]
+        public IActionResult UpdateComic()
+        {
+            return NotImplemented();
+        }
+
+        [HttpDelete("{comicId:int}"), Authorize]
+        public IActionResult DeleteComic(int comicId)
+        {
+            return NotImplemented();
         }
 
     }
