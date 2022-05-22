5d3ed5d39e012ab8a07626ce821f6b5425a51c73
Adam Sears <asears2@live.com>
2022-05-22 16:12:02 -0500

    Added enums

---
+++ WebComicProvider.Domain/StatusEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace WebComicProvider.Domain
+{
+    public enum StatusEnum
+    {
+        ComicActive = 1,
+        ComicInactive = 2,
+        ComicCompleted = 3,
+
+        IssueActive = 4,
+        IssueInactive = 5,
+        IssueCompleted = 6,
+
+        PageActive = 7,
+        PageInactive = 8,
+        PageCompleted = 9
+    }
+}
